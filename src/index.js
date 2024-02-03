170ef61f945580372a14154e760b91aee765a6f6
Adeebahmed333 <adeebahmed3337@gmail.com>
2024-02-03 16:34:46 +0530

    Implemented User Registration API includeing password hashing

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 const express=require('express');
 const {PORT}=require('./config/serverConfig');
+const bodyParser= require('body-parser');
+const apiRoutes=require('./routes/index');
 const app=express();
 
 const prepareAndStartServer=()=>{
+   //app.use(express.json());
+   app.use(bodyParser.json());
+   app.use(bodyParser.urlencoded({extended:true}));
+   app.use('/api',apiRoutes);
    app.listen(PORT,()=>{
     console.log(`Server Started on PORT: ${PORT}`);
    });
