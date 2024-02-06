1ab82ffe3bbc2be4387529f05a97a7fbf0f56279
Adeebahmed333 <adeebahmed3337@gmail.com>
2024-02-06 16:14:58 +0530

    Added jwt creation and verification logic

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -4,5 +4,6 @@ dotenv.config();//calls .env file
 
 module.exports={
     PORT:process.env.PORT,
-    SALT:bcrypt.genSaltSync(13)
+    SALT:bcrypt.genSaltSync(13),
+    JWT_KEY:process.env.JWT_KEY
 }
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,5 +1,6 @@
+const jwt=require('jsonwebtoken');
 const UserRepository=require('../repository/user-repository');
-
+const {JWT_KEY}=require('../config/serverConfig');
 class UserService{
 constructor()
 {
@@ -16,6 +17,25 @@ constructor()
         console.log(error);
     }
  }
+    createToken(user) {
+       try {
+        const result=jwt.sign(user,JWT_KEY,{expiresIn:'1d'});
+        return result;
+       } catch (error) {
+        console.log("Something Went Wrong In Token Creation");
+        console.log(error);
+       }
+    }
+    verifyToken(token)
+    {
+        try {
+            const response=jwt.verify(token,JWT_KEY);
+            return response;
+        } catch (error) {
+        console.log("Something Went Wrong In Token Validation");
+        console.log(error);
+        }
+    }
  
 }
 module.exports=UserService;
