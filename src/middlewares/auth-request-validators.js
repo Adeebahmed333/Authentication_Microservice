6e05ba2bbb455f001422cf11b4bad285065e0b0f
Adeebahmed333 <adeebahmed3337@gmail.com>
2024-02-06 17:08:44 +0530

    Added Email And Password Validation for Incomming Requests

---
+++ src/middlewares/auth-request-validators.js
@@ -0,0 +1,18 @@
+const validateUserAuth=(req,res,next)=>{
+    if(!req.body.email || !req.body.password)
+    {
+        return res.status(400).json({
+            success:false,
+            data:{},
+            message:"Something Went Wrong",
+            err:"Email or Password Missing in the request"
+        })
+
+    }
+    next();
+}
+
+module.exports={
+    validateUserAuth
+}
+
